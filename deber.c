4817d23d15105a8f06c19c7c64ef746cc895c417
Jordan.Carrillo <jordan.carrilo@udla.edu.ec>
2025-10-02 22:32:28 -0500

    inicio

---
+++ deber.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+ 
+int main()
+{
+    //ingrese cuantos estudiantes hay, ingrese las notas y saque el promedio]
+    int numEstudiantes, nota, suma;
+    float promedio;
+    printf("Ingrese el numero de estudiantes: ");
+    scanf("%d",&numEstudiantes);
+   while(numEstudiantes <= 0){
+       printf("el valor debe ser positivo\n");
+       printf("Ingrese el numero de estudiantes: ");
+        scanf("%d",&numEstudiantes);
+   }
+   for(int i = 0; i < numEstudiantes;i++){
+       printf("Estudiante %d\n",(i + 1));
+       do{
+           printf("Ingrese la nota, debe estar entre 1 y 10: ");
+           scanf("%d",&nota);
+           if(nota > 10 || nota < 1){
+               printf("La nota no es valida, intente nuevamente\n");
+           }
+       }while(nota > 10 || nota < 1);   
+   }
+  // printf("Tenemos %d estudiantes\n",numEstudiantes);
+ 
+    return 0;
+}
